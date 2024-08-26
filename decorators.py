4bd0337a3641064403b403473620acc5209bcc73
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-26 13:47:17 +0500

    Decorators

---
+++ decorators.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def be_awesome(name):
+    return f"Yo {name}, together we're cool!"
+
+def greet_bob(greet_func):
+    return greet_func("Bob")
+
