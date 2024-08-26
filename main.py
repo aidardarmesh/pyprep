ae0f761c6ed0d0e27728533ab76e813646761a8c
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-26 15:50:32 +0500

    added debug decorator

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from decorators import timer
+from decorators import timer, debug
 
 @timer
 def waste_some_time(num_times):
     for _ in range(num_times):
         sum([num ** 2 for num in range(10_000)])
 
-waste_some_time(1)
-waste_some_time(999)
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f"Howdy, {name}!"
+    else:
+        return f"Whoa, {name}! {age} already, growing up!"
+
+make_greeting("Benjamin")
+make_greeting("Juan", age=114)
