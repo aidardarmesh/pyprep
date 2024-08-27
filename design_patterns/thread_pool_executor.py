cfcbef777876991c9a4b2d3c18d87961c95f3ab5
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-27 11:54:00 +0500

    Thread Pool Design Pattern

---
+++ design_patterns/thread_pool_executor.py
@@ -0,0 +1,12 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def task(n):
+    print(f"Executing task {n}")
+    time.sleep(1)
+    print(f"Completed task {n}")
+
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for i in range(10):
+        executor.submit(task, i)
+
